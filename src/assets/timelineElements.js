dd7b5f5e0dcd934ea83aa4ef161e720fe8ab36f0
Arpi Patel <arpipatel96@gmail.com>
2025-05-28 14:49:47 -0400

    Update timelineElements.js

--- src/assets/timelineElements.js
+++ src/assets/timelineElements.js
@@ -2,7 +2,7 @@ let timelineElements = [
   {
     id: 1,
     title: "Full-Stack developer",
-    location: "FYMAB Inc,Mindsherpa.health Canada",
+    location: "FYMAB Inc, Mindsherpa.health, Canada",
     description:
       "At FYMAB Inc., I work as a Full-Stack Developer, utilizing technologies like HTML, CSS, JavaScript, React, and Python to build and maintain web applications. I focus on developing both front-end and back-end components, managing databases, and creating efficient API services.",
     date: "February 2025- May 2025",
